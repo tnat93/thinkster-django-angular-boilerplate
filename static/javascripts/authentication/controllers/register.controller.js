c1442e3b68456897195d75ccaaf9a5e2f75d2523
tnat93 <xoxo_tammy@hotmail.com>
2016-02-08 13:47:35 -0600

    Completed Tutorial

---
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -0,0 +1,65 @@
+/*
+* Register controller
+* @namespace thinkster.authentication.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.authentication.controllers')
+    .controller('RegisterController', RegisterController);
+
+  RegisterController.$inject = ['$location', '$scope', 'Authentication'];
+
+  /*
+  * @namespace RegisterController
+  */
+  function RegisterController($location, $scope, Authentication) {
+    var vm = this;
+
+    vm.register = register;
+
+    /*
+    * @name register
+    * @desc Register a new user
+    * @memberOf thinkster.authentication.controllers.RegisterController
+    */
+    function register(email, password, username) {
+      return $http.post('/api/v1/accounts/', {
+        username: username,
+        password: password,
+        email: email
+      }).then(registerSuccessFn, registerErrorFn);
+
+      /*
+      * @name registerSuccessFn
+      * @desc Log the new user in
+      */
+      function registerSuccessFn(data, status, headers, config) {
+        Authentication.login(email, password);
+      }
+
+      /*
+      * @name registerErrorFn
+      * @desc Log "Epic failure!" to the console
+      */
+      function registerErrorFn(data, status, headers, config) {
+        console.error('Epic failure!');
+      }
+    }
+
+    activate();
+
+    /*
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf thinkster.authentication.controllers.RegisterController
+     */
+    function activate() {
+      // If the user is authenticated, they should not be here.
+      if (Authentication.isAuthenticated()) {
+        $location.url('/');
+      }
+    }
+  }
+})();
